802532fb57b53b0d6bf60aa496828da33b530b7e
立石義弘 <115062998+tateishiyoshihiro@users.noreply.github.com>
2024-02-20 15:39:52 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,59 +1,37 @@
 #include<stdlib.h>
-#include<stdio.h>
-#include<Windows.h>
-#include<time.h>
+#include <Windows.h>
+#include <time.h>
 #include <functional>
 
-typedef void (*PFunc)(int*);
+void setTimeout(std::function<void(int*)> p, int& second) {
 
-// コールバック関数
-void DispResult(int* s) { printf("%d秒待って実行された\n", *s); }
+	p(&second);
+	Sleep(second * 1000);
+}
 
-//void setTimeout(PFunc p, int second) {
-//	// コールバック関数を呼び出す
-//	Sleep(second * 1000);
-//	// macとかのUnix系OSの場合
-//	// Sleep(second)
-//	p(&second);
-//}
+int main() {
 
-int main(int argc, const char* argv[]) { 
-	int num = 10;
-	std::function<void(int)> 
-		setTimeout = [](int second,PFunc p) { 
-		// コールバック関数を呼び出す };
-		Sleep(second * 1000);
-		// macとかのUnix系OSの場合
-		// Sleep(second)
-		p(&second);
+	std::function<void(int*)> fx = [](int* s) { printf("%d秒待って実行されたよ\n", *s); };
 
-	
-	// 1.抽選結果はランダムで決める
-	unsigned int curretTime = time(nullptr);
-	srand(curretTime);
+	srand((unsigned int)time(nullptr));
 
-	// 1~6の乱数
 	int dice = rand() % 6 + 1;
-	int result = dice % 2;
-	// 2.ユーザから奇数か偶数かの入力を受け付ける
+
 	int userAnswer = 0;
-	printf("奇数か偶数かを予想する、偶数なら0奇数なら1を入力\n");
+	int time = 3;
+
+	printf("奇数か偶数化を予想する、偶数なら　0, 奇数なら1を入力してください");
 	scanf_s("%d", &userAnswer);
 
-	// 3.
-	printf("start\n");
-	PFunc p;
-	p = DispResult;
-	setTimeout(p, 3);
-	printf("骰子の出目が%dのため\n", dice);
-	printf("%d", result);
-	if (result)
-		printf("半\n");
-	else
-		printf("丁\n");
-	if (result == userAnswer)
-		printf("正解");
-	else
-		printf("不正解");
+	// PFunc p;
+	// p = DispResult;
+	setTimeout(fx, time);
+
+	if (dice % 2 == userAnswer) {
+		printf("正解\n");
+	} else {
+		printf("不正解\n");
+	}
+
 	return 0;
 }
